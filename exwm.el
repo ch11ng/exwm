12e2d574c916ec7b93b645c0d0bf2e1fb0ce342a
Chris Feng <chris.w.feng@gmail.com>
2016-02-12 14:12:33 +0800

    Bump version to 0.2

--- exwm.el
+++ exwm.el
@@ -4,8 +4,8 @@
 
 ;; Author: Chris Feng <chris.w.feng@gmail.com>
 ;; Maintainer: Chris Feng <chris.w.feng@gmail.com>
-;; Version: 0.1
-;; Package-Requires: ((xelb "0.3"))
+;; Version: 0.2
+;; Package-Requires: ((xelb "0.5"))
 ;; Keywords: unix
 ;; URL: https://github.com/ch11ng/exwm
 
